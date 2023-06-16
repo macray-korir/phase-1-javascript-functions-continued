d806dc22aecdc6232d52601a417a064b26068f72
macray <macraykorir254@gmail.com>
2023-06-16 08:45:20 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // code your solution here
+// Define the saturdayFun function declaration
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // Define the mondayWork function expression
+  const mondayWork = function (activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // Define the wrapAdjective function
+  function wrapAdjective(flair = '*') {
+    return function (adjective) {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
+  
+  // Export the functions to be used in tests or other files
+  module.exports = {
+    saturdayFun,
+    mondayWork,
+    wrapAdjective,
+  };
+  
